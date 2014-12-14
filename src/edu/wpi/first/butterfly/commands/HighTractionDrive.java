fd03a50fe569160e24fda1bcdabd73e2609315a0
Tominator368 <thomas.schweich@gmail.com>
2014-12-14 15:12:44 -0600

    Made a custom function to fix the vector stuff and sectored off stuff for people to do.

--- src/edu/wpi/first/butterfly/commands/HighTractionDrive.java
+++ src/edu/wpi/first/butterfly/commands/HighTractionDrive.java
@@ -19,7 +19,7 @@ public class HighTractionDrive extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        //Fire the pneumatic cylinders
+        //TODO: Fire the pneumatic cylinders
     }
 
     // Called repeatedly when this Command is scheduled to run
